ab5dfa67b511928dff7fc4b46768d0b16c6e60fb
Muthurajesh123 <romanrajesh150604@gmail.com>
2024-12-01 16:09:03 +0530

    Add files via upload

---
+++ ntrl no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i=1, n, sum=0;
+//input
+
+printf("enter the n value:");
+scanf("%d",&n);
+//loop
+while(i<=n)
+{
+ sum=sum+i;
+ i++;
+}
+//ans
+printf("sum:%d",sum);
+}
